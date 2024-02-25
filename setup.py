1e9e8d936c5fc664d30a2a34ab33eb4435deaa46
Benjamin M <137508630+bzm10@users.noreply.github.com>
2024-02-25 18:40:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.5'
 
-DESCRIPTION = 'Python advanced color package'
+DESCRIPTION = 'A comprehensive Python package for advanced color manipulation and analysis'
+
+# Read the contents of the README.md file
+with open("README.md", "r", encoding="utf-8") as fh:
+    print(fh)
+    long_description = fh.read()
 
 # Setting up
 setup(
@@ -11,16 +16,18 @@ setup(
     author="Benjamin Mark",
     author_email="bzmarkovits@yahoo.com",
     description=DESCRIPTION,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[],
     keywords=["color conversion", "RGB", "HEX", "CMYK", "HSL", "LAB", "LUV", "XYZ", "YIQ", "HSV", "YUV", "YCbCr", "LCH", "LMS", "color toolkit", "color space", "Python color", "color manipulation", "color analysis", "color utility","python","color"],
     classifiers=[
-        "Development Status :: 1 - Released",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
-    ]
+    ],
+    license="MIT" 
 )
